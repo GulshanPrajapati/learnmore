b5e2aa30036543ff92cb79a591fa57f18a353c80
Gulshan Prajapati <60136202+GulshanPrajapati@users.noreply.github.com>
2022-03-25 10:53:28 +0530

    update

---
+++ js/auth.js
@@ -0,0 +1,50 @@
+// show loader on page load
+$(document).ready(function () {
+    // $(".loading").hide();
+});
+
+// initializeApp firebase
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBABYSKw9SNQ4zEHTP9wYC-gDVw_dy2XjI",
+    authDomain: "webdemo-c1945.firebaseapp.com",
+    databaseURL: "https://webdemo-c1945.firebaseio.com",
+    projectId: "webdemo-c1945",
+    storageBucket: "webdemo-c1945.appspot.com",
+    messagingSenderId: "448312953544",
+    appId: "1:448312953544:web:34a8cb12d55ec12cb933fd",
+    measurementId: "G-2C0N9ER52Z",
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+// on state change
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        var uid = user.uid;
+        console.log("Logged In");
+        localStorage.setItem('userId', uid);
+        //check user have goal set or not 
+        checkUserGoal();
+    } else {
+        window.location = "../login.html";
+    }
+});
+
+//function for checking user goal is set or not
+function checkUserGoal() {
+    var userId = localStorage.getItem('userId');
+    var docRef = db.collection(userId).doc("goals");
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            // console.log("Document data:", doc.data());
+            $('.loading').hide();
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            window.location = window.location['origin']+"/pages/addgoal.html";
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
--- js/index.js
+++ js/index.js
@@ -1,58 +1,59 @@
-// // show loader on page load
-$( document ).ready(function() {
-  $('.loading').hide();
-});
+// // // show loader on page load
+// $(document).ready(function () {
+//   // $('.loading').hide();
+// });
 
 
-// initializeApp
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBABYSKw9SNQ4zEHTP9wYC-gDVw_dy2XjI",
-  authDomain: "webdemo-c1945.firebaseapp.com",
-  databaseURL: "https://webdemo-c1945.firebaseio.com",
-  projectId: "webdemo-c1945",
-  storageBucket: "webdemo-c1945.appspot.com",
-  messagingSenderId: "448312953544",
-  appId: "1:448312953544:web:34a8cb12d55ec12cb933fd",
-  measurementId: "G-2C0N9ER52Z",
-});
-const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
+// // initializeApp
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyBABYSKw9SNQ4zEHTP9wYC-gDVw_dy2XjI",
+//   authDomain: "webdemo-c1945.firebaseapp.com",
+//   databaseURL: "https://webdemo-c1945.firebaseio.com",
+//   projectId: "webdemo-c1945",
+//   storageBucket: "webdemo-c1945.appspot.com",
+//   messagingSenderId: "448312953544",
+//   appId: "1:448312953544:web:34a8cb12d55ec12cb933fd",
+//   measurementId: "G-2C0N9ER52Z",
+// });
+// const db = firebaseApp.firestore();
+// const auth = firebaseApp.auth();
+// // on state change
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     var uid = user.uid;
+//     console.log(user.phoneNumber ? user.phoneNumber : user.email);
+//     console.log(uid);
+//     console.log("Logged In");
+//     localStorage.setItem('userId', uid);
+
+//     //check user have goal set or not 
+//     checkUserGoal();
+//     // $('.loading').hide();
+//   } else {
+//     window.location = "login.html";
+//   }
+// });
 
 // logout 
-$('#logout').click(function(){
+$('#logout').click(function () {
   firebase.auth().signOut().then(() => {
-      // Sign-out successful.
-      window.location.reload();
-    }).catch((error) => {
-      // An error happened.
-    });
+    // Sign-out successful.
+    window.location.reload();
+  }).catch((error) => {
+    // An error happened.
+  });
 })
-
-
-// on state change
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    var uid = user.uid;
-    console.log(user.phoneNumber ? user.phoneNumber : user.email);
-    console.log(uid);
-    console.log("Logged In");
-    // $('.loading').hide();
-  }else{
-      window.location="login.html";
-  }
-});
-
-
-
 
 // message icon onclick css 
-$('.message_icon').click(function(){
-  $('.message_icon').css('background-color','rgba(68, 68, 68, 0.8)')
+$('.message_icon').click(function () {
+  $('.message_icon').css('background-color', 'rgba(68, 68, 68, 0.8)')
+})
+
+// toggling goal module 
+$(".m_head").on('click', function () {
+  $(this).parent().find(".module_data").toggle();
+  $(this).parent().find('.icon').toggleClass('rotate_icon')
 })
 
 
-// toggling goal module 
-$(".m_head").on('click',function(){
-  $(this).parent().find(".module_data").toggle();
-  $(this).parent().find('.icon').toggleClass('rotate_icon')
-}) 
+
