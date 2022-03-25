aa38c9579523d229701c1a6d0ac2ac48e62eac8f
Umesh Jaiswar <55977345+jaiswarrahul29@users.noreply.github.com>
2022-03-25 10:31:42 +0530

    Update addgoal.js

--- js/addgoal.js
+++ js/addgoal.js
@@ -33,3 +33,97 @@ firebase.auth().onAuthStateChanged((user) => {
     window.location = "../login.html";
   }
 });
+
+
+//fetch course data
+function getCourseData() {
+  var userId = localStorage.getItem('userId');
+  db.collection("courses")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id);
+        $('#course_data').append('<option value="' + doc.id + '"></option>');
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+}
+getCourseData();
+
+//set goal function
+function setGoal(goalname, course, module, duration, time) {
+  var userId = localStorage.getItem('userId');
+  var goals = db.collection(userId).doc("goals").collection(goalname).doc(goalname);
+  goals.set({
+    course: course,
+    duration: duration,
+    module: module,
+    time: time
+  })
+    .then(() => {
+      console.log("Document successfully updated!");
+      window.location = "./addmodule.html?goal=" + goalname + "";
+    })
+    .catch((error) => {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+}
+
+//on next button clicked
+function nextButtonClick() {
+
+  $('#submit').click(function () {
+    var goalname = $("#goal_name").val();
+    var course = $("#course").val();
+    var duration = $("#duration").val();
+    var time = $("#time").val();
+
+    if (goalname == '') {
+      $("#goal_name").css('border', '1px solid red');
+      setTimeout(function () {
+        $("#goal_name").css('border', 'none');
+      }, 2000)
+    }
+    if (course == '') {
+      $("#course").css('border', '1px solid red');
+      setTimeout(function () {
+        $("#course").css('border', 'none');
+      }, 2000)
+    }
+    if (duration == '') {
+      $("#duration").css('border', '1px solid red');
+      setTimeout(function () {
+        $("#duration").css('border', 'none');
+      }, 2000)
+    }
+    if (time == '') {
+      $("#time").css('border', '1px solid red');
+      setTimeout(function () {
+        $("#time").css('border', 'none');
+      }, 2000)
+    }
+
+    if (goalname != '' && course != '' && duration != '' && time != '') {
+      var getmodule = db.collection("courses").doc(course);
+      getmodule.get().then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          var module = doc.data();
+          setGoal(goalname, course, module, duration, time);
+        } else {
+          var module = "";
+          setGoal(goalname, course, module, duration, time);
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+    }
+  })
+}
+
+
+nextButtonClick()
